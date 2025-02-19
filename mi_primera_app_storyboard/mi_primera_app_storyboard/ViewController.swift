dd951ff1c9af1028659533338112360a5294d6e6
Jadzia Eleonore <bluakorvo@protonmail.com>
2025-02-19 10:29:00 -0700

    [PRIMER] comit para iniciar este repo

---
+++ mi_primera_app_storyboard/mi_primera_app_storyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mi_primera_app_storyboard
+//
+//  Created by Jadzia Gallegos on 19/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
