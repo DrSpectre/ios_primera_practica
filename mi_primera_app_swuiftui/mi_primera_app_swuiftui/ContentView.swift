c49cca52ed03651a294d2036a722ca125544c1ac
Jadzia Eleonore <bluakorvo@protonmail.com>
2025-02-21 09:51:13 -0700

    [AGREGA] un proyecto de hola mundo usando swiftui

---
+++ mi_primera_app_swuiftui/mi_primera_app_swuiftui/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  mi_primera_app_swuiftui
+//
+//  Created by Jadzia Gallegos on 21/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var texto_introducido_por_el_usuario: String = "Place holder"
+    @State private var texto_a_mostrar_en_saludo: String = "mundo"
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hola \(texto_a_mostrar_en_saludo)")
+            
+            TextEditor(text: $texto_introducido_por_el_usuario)
+            
+            Button("¡PULSAME!", action: {
+                if(texto_introducido_por_el_usuario == ""){
+                    texto_a_mostrar_en_saludo = "mundo"
+                }
+                else{
+                    texto_a_mostrar_en_saludo = texto_introducido_por_el_usuario
+                    texto_introducido_por_el_usuario = ""
+                }
+            })
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mi_primera_app_swuiftui/mi_primera_app_swuiftui/mi_primera_app_swuiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  mi_primera_app_swuiftuiApp.swift
+//  mi_primera_app_swuiftui
+//
+//  Created by Jadzia Gallegos on 21/02/25.
+//
+
+import SwiftUI
+
+@main
+struct mi_primera_app_swuiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
